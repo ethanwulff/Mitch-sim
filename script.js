23dc4a4371ef124400aa21c23cdf742f190772ed
ethanwulff <ethanwulff97@gmail.com>
2019-09-18 23:48:55 -0700

    added mood

---
+++ script.js
@@ -0,0 +1,35 @@
+let dollars = 0;
+
+let sadness = 0;
+
+let button = document.getElementById("work");
+
+let dollars = document.getElementById("dollars");
+
+let mood = document.getElementById("mood");
+
+let img = document.getElementById("ubereats");
+
+
+
+// work event listener
+button.addEventListener("click", function () {
+    console.log("clicked");
+    dollars++;
+    mood -= 1;
+    console.log(dollars);
+    console.log(mood);
+    dollars.innerHTML = "dollards: " + dollars;
+});
+
+// eats even listener
+img.addEventListener("click", function () {
+    if (dollars >= 5) {
+        dollars -= 5;
+
+        console.log("eats");
+
+        console.log(dollars);
+        dollars.innerHTML = "dollards: " + dollars;
+    }
+}); 
